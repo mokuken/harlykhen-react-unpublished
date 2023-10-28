c853deba93eba3d9290f68ca0c17cfdc76ba44dd
mokuken <imsilencer44@gmail.com>
2023-10-28 11:07:16 +0800

    added contact contents

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
             <h3>all you need to know</h3>
             <h1>ABOUT ME</h1>
             <br />
-            <div className="description">
+            <div className="content-padding">
                 <p>Hello visitor, welcome to my website, my name is Harly Khen also known as Moku Ken in the world of internet, im 20 years old taking computer science in college and focusing on self-improvement.</p>
                 <br />
                 <p>i see you wanna know more about me, i don't really talk alot about my selft because im a bit shy person, thats where i put Moku Ken to handle it, so i will start about what is my favorate things...</p>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,26 @@
 import React from "react";
+import '../styles/Contact.css';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact-container">
             <h3>if you have business, you have my</h3>
             <h1>CONTACT</h1>
+            <br />
+            <div className="contact-coloumn contact-padding">
+                <a href="https://harlykhen@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-box card">
+                    <i class="fa-brands fa-envelope"></i> Email
+                </a>
+                <a href="https://github.com/mokuken" target="_blank" rel="noopener noreferrer" className="contact-box card">
+                    <i class="fa-brands fa-github-alt"></i> Github
+                </a>
+                <a href="https://m.facebook.com/people/Harly-Khen-V-Quimelat/100034130554707/?comment_id=Y29tbWVudDoxMTUwODQyMTM1NzY0MjhfMTE2NDQwMjYzNDQwODIz" target="_blank" rel="noopener noreferrer" className="contact-box card">
+                    <i class="fa-brands fa-facebook"></i> Facebook
+                </a>
+                <a href="https://www.linkedin.com/in/harlykhen" target="_blank" rel="noopener noreferrer" className="contact-box card">
+                    <i class="fa-brands fa-linkedin"></i> Linkedin
+                </a>
+            </div>
         </div>
     )
 }
