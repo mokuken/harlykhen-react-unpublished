2cf4b925eb5baf869b1b5be6a6e0b43fa294de33
mokuken <imsilencer44@gmail.com>
2023-11-01 14:11:51 +0800

    compressed gallery images and converted to webp

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,12 +1,12 @@
 import React from "react";
 import '../styles/Contact.css';
-import pic1 from '../assets/image/gallery/pic1.png'
-import pic2 from '../assets/image/gallery/pic2.png'
-import pic3 from '../assets/image/gallery/pic3.png'
-import pic4 from '../assets/image/gallery/pic4.png'
-import pic5 from '../assets/image/gallery/pic5.png'
-import pic6 from '../assets/image/gallery/pic6.png'
-import pic7 from '../assets/image/gallery/pic7.png'
+import pic1 from '../assets/image/gallery/pic1.webp'
+import pic2 from '../assets/image/gallery/pic2.webp'
+import pic3 from '../assets/image/gallery/pic3.webp'
+import pic4 from '../assets/image/gallery/pic4.webp'
+import pic5 from '../assets/image/gallery/pic5.webp'
+import pic6 from '../assets/image/gallery/pic6.webp'
+import pic7 from '../assets/image/gallery/pic7.webp'
 
 const Contact = () => {
     return (
