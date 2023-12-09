1e56791c75fa775c81a4a6ece31ab265fa767124
mokuken <imsilencer44@gmail.com>
2023-12-10 00:29:46 +0800

    new about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,7 @@
 import React from "react";
+import LoadImage from '../components/LoadImage.js';
+import me from '../assets/image/me.gif'
+import '../styles/About.css';
 
 const About = () => {
     return (
@@ -7,18 +10,13 @@ const About = () => {
             <h1>ABOUT ME</h1>
             <br />
             <div className="content-padding">
-                <p><strong>Hello friend, welcome to my place!</strong> You may already know my name but i also known as Moku Ken, my 2nd perosnality that i created, im 20 years old taking 4 years of computer science in college and i'm currently focusing on my self-improvement.</p>
-                <br />
-                <p>i see you wanna know more about me, i don't really talk alot about my selft because im a bit shy person, thats where i put Moku Ken to handle it, so i will start about what is my favorate things...</p>
-                <br />
-                <p>First thing first i love Technology, you can do alot of thing using it.</p>
-                <p>Second is Coding, theres no reason for it i just love creating program stuff.</p>
-                <p>Third Video Editing, the reason? nahh i just love creating my own dopamine :D.</p>
-                <p>and Fourth of course Anime who does not love anime everyone loves it, wait do they?</p>
-                <p>Fifth Read Books, my favorate topic to read is about stoicism</p>
-                <p>Sixth, theres no sixth this is enough XD</p>
-                <br />
-                <p>This website is dedicated for sharing cool stuff and the learnings along the way.</p>
+                <div className="about-main-container">
+                    <LoadImage src={me} alt={Image}/>
+                    <p><h1>Hello friend,</h1>welcome to my place! You may already know my name but i also known as Moku Ken, my 2nd perosnality that i created, im 20 years old taking 4 years of computer science in college and i'm currently focusing on my self-improvement and improving my programming skills.
+                    <hr />
+                    As a young person just starting out on life, there are a lot of things I don't know yet. But that's ok - there's always room for improvement. And that's what this website is all about - sharing my experiences, successes, and mistakes so that you can gain the knowledge and skills you need to be successful too.
+                    </p>
+                </div>
             </div>
         </div>
     )
