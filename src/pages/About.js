c387af8c239786673862e9fb11b8e860d7f0ce49
mokuken <imsilencer44@gmail.com>
2023-12-14 01:00:16 +0800

    new project page content w/ little adjustments

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
-import LoadImage from '../components/LoadImage.js';
 import me from '../assets/image/me.gif'
 import '../styles/About.css';
+import LoadImage from '../components/LoadImage.js';
 
 const About = () => {
     return (
@@ -11,11 +11,15 @@ const About = () => {
             <br />
             <div className="content-padding">
                 <div className="about-main-container">
-                    <LoadImage src={me} alt={Image}/>
-                    <p><h1>Hello friend,</h1>welcome to my place! You may already know my name but i also known as Moku Ken, my 2nd perosnality that i created, im 20 years old taking 4 years of computer science in college and i'm currently focusing on my self-improvement and improving my programming skills.
-                    <hr />
-                    As a young person just starting out on life, there are a lot of things I don't know yet. But that's ok - there's always room for improvement. And that's what this website is all about - sharing my experiences, successes, and mistakes so that you can gain the knowledge and skills you need to be successful too.
-                    </p>
+                    <LoadImage src={me}/>
+                    <article>
+                        <h1>Hello friend,</h1>
+                        <br />
+                        <p>welcome to my place! You may already know my name but i also known as Moku Ken, my 2nd perosnality that i created, im 20 years old taking 4 years of computer science in college and i'm currently focusing on my self-improvement and improving my programming skills.</p>
+                        <br />
+                        <p>As a young person just starting out on life, there are a lot of things I don't know yet. But that's ok - there's always room for improvement. And that's what this website is all about - sharing my experiences, successes, and mistakes so that you can gain the knowledge and skills you need to be successful too.
+                        </p>
+                    </article>
                 </div>
             </div>
         </div>
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,64 +1,38 @@
 import React from "react";
-import ProjectImageSourceOne from '../assets/image/projects.webp'
-import ProjectImageSourceTwo from '../assets/image/project-2.webp'
+import '../styles/Project.css';
 
 const Project = () => {
     return (
-        <div>
+        <div className="project-container">
             <h3>some of my recent work</h3>
             <h1>PROJECTS</h1>
             <br />
             <div className="content-padding">
-                <div className="projCont">
-                    <div className="card card-left">
-                        <img src={ProjectImageSourceOne} alt="" />
-                        <h3>Dynamic Window Manager (DWM)</h3>
-                        <p>My patched version of suckless dynamic window manage.</p>
+                <div className="project-folders">
+                    <div className="project-row">
+                        <div className="card">
+                            <h2>My Dynamic Window Manager</h2>
+                            <p>My patched version of suckless dynamic window manage.</p>
+                        </div>
+                        <div className="card">
+                            <h2>Lost Dream Game</h2>
+                            <p>A game that i made based on the things i remember to my dreams.</p>
+                        </div>
+                        <div className="card">
+                            <p>empty</p>
+                        </div>
                     </div>
-                    <div className="card card-middle">
-                        <img src={ProjectImageSourceTwo} alt="" />
-                        <h3>Lost Dream Game</h3>
-                        <p>
-                            A game that i made based on the things i remember to my dreams.
-                        </p>
-                    </div>
-                    <div className="card card-right">
-                        <img
-                        className="inactive"
-                        src={ProjectImageSourceOne}
-                        alt=""
-                        />
-                        <h3>Empty</h3>
-                        <p>Nothing its Empty</p>
-                    </div>
-                </div>
-                <div className="projCont">
-                    <div className="card card-left">
-                        <img
-                        className="inactive"
-                        src={ProjectImageSourceOne}
-                        alt=""
-                        />
-                        <h3>Empty</h3>
-                        <p>Nothing its Empty</p>
-                    </div>
-                    <div className="card card-middle">
-                        <img
-                        className="inactive"
-                        src={ProjectImageSourceOne}
-                        alt=""
-                        />
-                        <h3>Empty</h3>
-                        <p>Nothing its Empty</p>
-                    </div>
-                    <div className="card card-right">
-                        <img
-                        className="inactive"
-                        src={ProjectImageSourceOne}
-                        alt=""
-                        />
-                        <h3>Empty</h3>
-                        <p>Nothing its Empty</p>
+                    <div className="project-row">
+                        <div className="card">
+                            <p>empty</p>
+                        </div>
+                        <div className="card">
+                            <h2>Library Login System</h2>
+                            <p>A login system where it record the daily login of students in the library everyday with cool features.</p>
+                        </div>
+                        <div className="card">
+                            <p>empty</p>
+                        </div>
                     </div>
                 </div>
             </div>
